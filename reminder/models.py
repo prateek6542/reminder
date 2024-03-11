d0fb68b0479480221e55182725d086ab5aada856
inspire1805 <inspireuplift1805@gmail.com>
2024-03-11 16:00:21 +0530

    Initial Commit

---
+++ reminder/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Reminder(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    message = models.TextField()
+    remind_via = models.CharField(max_length=50, choices=[('SMS', 'SMS'), ('Email', 'Email')])
+    
+    
+    def __str__(self):
+        return self.message
